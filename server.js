ae71df9af1f25ebae481cf9b8025540a82d304e0
Waldemar Szymon <walldy@tlen.pl>
2023-06-12 18:05:11 +0200

    Use handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use((req, res, next) => {
   res.show = (name) => {
@@ -28,8 +32,12 @@ app.get('/info', (req, res) => {
   res.show('info.html');
 });
 
-app.get('/history', (req, res, next) => {
+app.get('/history', (req, res) => {
   res.show('history.html');
+});
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 app.use((req, res) => {
