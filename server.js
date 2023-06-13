34e0cd142883a751e182b97b4fe803b89561f964
Waldemar Szymon <walldy@tlen.pl>
2023-06-13 12:52:39 +0200

    Change files extensions

--- server.js
+++ server.js
@@ -7,33 +7,26 @@ const app = express();
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
-
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index', { layout: false });
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about', { layout: false });
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact', { layout: false });
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info.', { layout: false });
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history', { layout: false });
 });
 
 app.get('/hello/:name', (req, res) => {
